eed16552c383f3c40c60084355ed03cc4a5ac791
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-03-24 15:05:41 -0300

    Colocando a função que troca de scena no singleplayer

--- Blue Pen Horror History 3/Assets/Scripts/SairJogo.cs
+++ Blue Pen Horror History 3/Assets/Scripts/SairJogo.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SairJogo : MonoBehaviour{
     public void Sair(){
         Application.Quit();
     }
 
+    public void CarregarCena(string cena){
+        SceneManager.LoadScene(cena);
+    }
 }
