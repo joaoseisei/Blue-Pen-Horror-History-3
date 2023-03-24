f37251e08bc646ca06102bd35e0cfcff66fa7dbf
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-03-24 14:18:47 -0300

    Adicionei o botao de sair do jogo, os creditos e todos os audios do menu

---
+++ Blue Pen Horror History 3/Assets/Scripts/SairJogo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SairJogo : MonoBehaviour{
+    public void Sair(){
+        Application.Quit();
+    }
+
+}
