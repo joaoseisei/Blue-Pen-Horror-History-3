f424ca93dfeb81f0212bb3d50338a902417f7b3c
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-03-24 17:39:46 -0300

    tirando os demos e adicionando o playercontroller

---
+++ Blue Pen Horror History 3/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerManager : MonoBehaviour{
+
+    PhotonView PV;
+
+    void Awake(){
+        PV = GetComponent<PhotonView>();
+    }
+
+    void Start(){
+       if(PV.IsMine){
+        CreateController();
+       } 
+    }
+
+    void CreateController(){
+
+    }
+}
